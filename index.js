9e423f5ad58f9313ecd628ebc4b03fc6ea43c6bb
Samantha Hyatt <shhyatt@gmail.com>
2019-07-27 02:34:33 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,13 @@
+function binaryMatch(letters){
+  let sortedLetters = letters.sort();
+  let frequency = {};
+  for (let i = 0; i < sortedLetter.length; i++){
+    let char = sortedLetter.charAt(i)
+    if(frequency[char]){
+      frequency[char]++
+    }else{
+      frequency[char] = 1
+    }
+  }
+  
+}
